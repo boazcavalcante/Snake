cf6de7652affd74a611aee8010108dadd2d0126f
Boaz Cavalcante <boazcavalcante1000@gmail.com>
2020-05-02 14:04:16 -0300

    Snake moving

--- script.js
+++ script.js
@@ -2,22 +2,45 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+let direction = "up";
+
 snake[0] = {
   x: 8 * box,
   y: 8 * box,
 }
 
-function createFrame() {
+function renderFrame() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function createSnake() {
+function renderSnake() {
   for (let i = 0; i < snake.length; i++) {
     context.fillStyle = "green";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
 
-createFrame();
-createSnake();
+function updateScreen() {
+  renderFrame();
+  renderSnake();
+
+  let snakeX = snake[0].x
+  let snakeY = snake[0].y
+
+  if(direction == "right") snakeX += box;
+  if(direction == "left") snakeX -= box
+  if(direction == "up") snakeY -= box;
+  if(direction == "down") snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead);
+}
+
+let game = setInterval(updateScreen, 2000);
