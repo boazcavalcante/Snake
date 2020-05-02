5f1aead7f3d2adccdfefb1b1daa8ee1f4759ae59
Boaz Cavalcante <boazcavalcante1000@gmail.com>
2020-05-02 14:37:08 -0300

    Direction adjustment

--- script.js
+++ script.js
@@ -21,19 +21,36 @@ function renderSnake() {
   }
 }
 
+document.addEventListener('keydown', event => {
+  if (event.keyCode == 37 && direction != "right") direction = "left";
+  if (event.keyCode == 38 && direction != "down") direction = "up";
+  if (event.keyCode == 39 && direction != "left") direction = "right";
+  if (event.keyCode == 40 && direction != "up") direction = "down";
+})
+
+function adjustDirection() {
+  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if (snake[0].x < 0 && direction == "left") snake[0].x = 15 * box;
+  if (snake[0].y < 0 && direction == "up") snake[0].y = 15 * box;
+  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+}
+
 function updateScreen() {
+
+  adjustDirection();
+
   renderFrame();
   renderSnake();
 
   let snakeX = snake[0].x
   let snakeY = snake[0].y
 
-  if(direction == "right") snakeX += box;
-  if(direction == "left") snakeX -= box
-  if(direction == "up") snakeY -= box;
-  if(direction == "down") snakeY += box;
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box;
 
-  snake.pop();
+  // snake.pop();
 
   let newHead = {
     x: snakeX,
@@ -43,4 +60,4 @@ function updateScreen() {
   snake.unshift(newHead);
 }
 
-let game = setInterval(updateScreen, 2000);
+let game = setInterval(updateScreen, 500);
